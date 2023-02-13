d2a9ae992212aa7e9ecf7cfee4788b07fa0880ec
Tung N Le <tung.le0319@gmail.com>
2023-02-12 19:07:16 -0700

    testing

---
+++ client/components/SideBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+export default function SideBar() {
+  return (
+    <div
+      className="    "
+    >
+      <div className=" group">
+        <img
+          src="https://images.unsplash.com/photo-1546182208-1e70985e2bf3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80"
+          alt=""
+          width={40}
+        />
+        <span className="  ">testing</span>
+      </div>
+
+   
+    </div>
+  );
+}
+
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+    },
   },
   plugins: [],
 };
