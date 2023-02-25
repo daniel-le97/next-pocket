1eb5eb339ffd8d6f0fb258ceb8b618762df9a4d1
daniel-le97 <daniel97.le@gmail.com>
2023-02-25 12:23:40 -0700

    update logout [sidebar]

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,68 +2,59 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import SideBar from '../components/SideBar'
-import ChannelsBar from '../components/ChannelsBar'
-import ContentContainer from '../components/ContentContainer'
-import React, {useEffect} from "react";
+import SideBar from "../components/SideBar";
+import ChannelsBar from "../components/ChannelsBar";
+import ContentContainer from "../components/ContentContainer";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { AppState } from "../../AppState";
 import { authsService } from "../services/AuthsService";
 import { pb } from "../../utils/pocketBase";
 const Home: NextPage = () => {
-const router = useRouter()
-useEffect(() => {
-  const user = pb.authStore.model
-  if(!user){
-    router.push('/login')
-  }
+  const router = useRouter();
+  useEffect(() => {
+    const user = pb.authStore.model;
+    if (!user) {
+      router.push("/login");
+    }
+  }, [router]);
 
-}, [router])
-
-
-
-useEffect(() => {
-  const user = pb.authStore.model;
-  const subscribe = async () => {
-    const res = await pb
-      .collection("usersStatus")
-      .getFirstListItem(`userId = "${user?.id}"`);
-    // console.log(res);
-
-//     // Update isOnline status to true on component mount
-//     const data = {
-//       userId: user?.id,
-//       isOnline: true,
-//     };
-//     const updatedRecord = await pb
-//       .collection("usersStatus")
-//       .update(res.id, data);
-//     // console.log(updatedRecord);
-
-    // Update isOnline status to false on beforeunload event
-    const handleBeforeUnload = () => {
-      const data = {
-        userId: user.id,
-        isOnline: false,
-      };
-      const updatedRecord = pb
+  useEffect(() => {
+    const user = pb.authStore.model;
+    const subscribe = async () => {
+      const res = await pb
         .collection("usersStatus")
-        .update(res.id, data);
-      console.log(updatedRecord);
+        .getFirstListItem(`userId = "${user?.id}"`);
+      // console.log(res);
+
+      //     // Update isOnline status to true on component mount
+      //     const data = {
+      //       userId: user?.id,
+      //       isOnline: true,
+      //     };
+      //     const updatedRecord = await pb
+      //       .collection("usersStatus")
+      //       .update(res.id, data);
+      //     // console.log(updatedRecord);
+
+      // Update isOnline status to false on beforeunload event
+      const handleBeforeUnload = () => {
+        const data = {
+          userId: user.id,
+          isOnline: false,
+        };
+        const updatedRecord = pb.collection("usersStatus").update(res.id, data);
+        console.log(updatedRecord);
+      };
+      window.addEventListener("beforeunload", handleBeforeUnload);
+
+      // Remove the event listener on unmount
+      return () => {
+        window.removeEventListener("beforeunload", handleBeforeUnload);
+      };
     };
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-//     // Remove the event listener on unmount
-//     return () => {
-//       window.removeEventListener("beforeunload", handleBeforeUnload);
-//     };
-//   };
-//   subscribe();
-// }, []);
-
-
-
-
+    subscribe();
+  }, []);
 
   return (
     <>
@@ -74,10 +65,9 @@ useEffect(() => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center ">
         <div className="flex  w-full ">
-          
-       <SideBar  />
-       <ChannelsBar/>
-       <ContentContainer/>
+          <SideBar />
+          <ChannelsBar />
+          <ContentContainer />
         </div>
       </main>
     </>
