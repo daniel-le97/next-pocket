8c8d6629c7290c211c6c49fe2098d1224f3515d1
daniel-le97 <daniel97.le@gmail.com>
2023-03-01 00:36:23 -0700

    types are hard

--- client/src/pages/AccountPage.tsx
+++ client/src/pages/AccountPage.tsx
@@ -1,18 +1,30 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { observer } from "mobx-react";
+import { Admin, Record } from "pocketbase";
 import React, { useEffect, useState } from "react";
+import { ServersResponse } from "../../pocketbase-types";
 // import { AppState } from "../../AppState.js";
 import { pb } from "../../utils/pocketBase";
 import { serversService } from "../services/ServersService";
 
 function AccountPage() {
-  const [user, setUser] = useState()
-  const [servers, setServers] = useState()
+  const [user, setUser] = useState<Record | Admin | null>()
+  const [servers, setServers] = useState<ServersResponse<unknown>>()
   useEffect(() => {
     console.log(pb.authStore.model);
-    setUser(pb.authStore.model);
+    const currentUser = pb.authStore.model
+    if (currentUser) {
+      setUser(currentUser);
+      const getServers = async () => {
+        const servers = await serversService.getUserServers(currentUser.id)
+        setServers(servers)
+      }
+      getServers()
+    }
     serversService.getUserServers(pb.authStore.model?.id).then(value => setServers(value))
-  }, []);
+  }, [user]);
 
   return (
     <div className="account-page">
