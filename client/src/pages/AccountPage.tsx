35f4addf47f24627db3fa38d17a9b135b43d33ec
Tung N Le <tung.le0319@gmail.com>
2023-03-07 15:31:50 -0700

    account

--- client/src/pages/AccountPage.tsx
+++ client/src/pages/AccountPage.tsx
@@ -5,9 +5,13 @@ import { observer } from "mobx-react";
 import { NextPage } from "next";
 import type { Admin, Record } from "pocketbase";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useUser } from "../../hooks/User";
 import type { ServersResponse } from "../../pocketbase-types";
 // import { AppState } from "../../AppState.js";
 import { pb } from "../../utils/pocketBase";
+import Pop from "../../utils/Pop";
+import { accountsService } from "../services/AccountsService";
 import { serversService } from "../services/ServersService";
 
 function AccountPage() {
@@ -33,7 +37,7 @@ function AccountPage() {
     <main className="  min-h-screen dark:bg-zinc-800  ">
       <div className="account-page">
         <div className="card  flex flex-col  justify-center   ">
-          <div className=" p-5 text-center flex justify-center ">
+          <div className=" flex justify-center p-5 text-center ">
             <img
               src={user?.avatarUrl}
               alt="Profile Image"
@@ -49,4 +53,42 @@ function AccountPage() {
   );
 }
 
+const EditAccount = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      emailVisibility: false,
+
+      avatarUrl: "",
+    },
+  });
+  const onSubmit = async (data: any) => {
+    try {
+      await accountsService.updateAccount(data);
+      reset();
+    } catch (error) {
+      console.error("createServer", error);
+
+      // await uploadService.deleteFile(data.image);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="bg-zinc-700 p-3">
+        <input type="text" />
+      </div>
+      <div className="bg-zinc-700 p-2">
+        <input type="checkbox" />
+      </div>
+    </form>
+  );
+};
 export default observer(AccountPage);
--- client/src/services/AccountsService.ts
+++ client/src/services/AccountsService.ts
@@ -2,8 +2,8 @@ import { pb } from "../../utils/pocketBase";
 import type { Account } from "../models/Account";
 
 class  AccountsService {
- async updateAccount(data: Account){
-  const id = pb.authStore.model.id
+ async updateAccount(data:useer){
+
     const account = await pb.collection('users').update(id, data)
     return account
   }
