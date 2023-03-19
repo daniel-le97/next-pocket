4159de816a176c2f00899199aa55529e84caf4d6
daniel-le97 <daniel97.le@gmail.com>
2023-03-19 15:01:53 -0600

    trying to reverse infinite scroll

--- client/src/components/Messages/MessageScroll.tsx
+++ client/src/components/Messages/MessageScroll.tsx
@@ -18,23 +18,25 @@ const MessageScroll = () => {
   };
   return (
     <div
-      className=""
       id="scrollableDiv"
-     
+      style={{
+        height: 300,
+        overflow: "auto",
+        display: "flex",
+        flexDirection: "column-reverse",
+      }}
     >
+      {/*Put the scroll bar always on the bottom*/}
       <InfiniteScroll
         dataLength={AppState.messages.length}
         next={fetchMore}
-    
-        hasMore={AppState.totalPages != AppState.page}
-        loader={<div>loading</div>}
-        endMessage={
-          <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
+        style={{ display: "flex", flexDirection: "column-reverse" }} //To put endMessage and loader to the top.
+        inverse={true} //
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        scrollableTarget="scrollableDiv"
       >
-        {AppState.messages.map((message, index) => (
+           {AppState.messages.map((message, index) => (
           <div key={index}>
             {
               <MessageCard
@@ -43,12 +45,41 @@ const MessageScroll = () => {
                 index={index}
               />
             }
-          {/* {new Date(message.created).toLocaleString()} */}
-          {message.id}
+            {/* {new Date(message.created).toLocaleString()} */}
+            {message.id}
           </div>
         ))}
       </InfiniteScroll>
     </div>
+    // <div className="" id="scrollableDiv">
+    //   <InfiniteScroll
+    //     dataLength={AppState.messages.length}
+    //     next={fetchMore}
+    //     hasMore={AppState.totalPages != AppState.page}
+    //     loader={<div>loading</div>}
+    //     // style={{ display: "flex", flexDirection: "column-reverse" }} //To put endMessage and loader to the top.
+    //     // inverse={true} //
+    //     endMessage={
+    //       <p style={{ textAlign: "center" }}>
+    //         <b>Yay! You have seen it all</b>
+    //       </p>
+    //     }
+    //   >
+    //     {AppState.messages.map((message, index) => (
+    //       <div key={index}>
+    //         {
+    //           <MessageCard
+    //             messages={AppState.messages}
+    //             message={message}
+    //             index={index}
+    //           />
+    //         }
+    //         {/* {new Date(message.created).toLocaleString()} */}
+    //         {message.id}
+    //       </div>
+    //     ))}
+    //   </InfiniteScroll>
+    // </div>
   );
 };
 
