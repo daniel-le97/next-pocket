f8202dffed65a7375144a1a45c91bdffb9e5aa0f
daniel-le97 <daniel97.le@gmail.com>
2023-03-21 22:09:22 -0600

    convert script to node

--- client/preview.js
+++ client/preview.js
@@ -1,5 +1,19 @@
-import { execSync } from "child_process";
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { execSync } = require("child_process")
+const {config} = require("dotenv")
 
 
-const hello = () => execSync('ls', {stdio: 'inherit'})
-hello()
+
+// Load environment variables from .env file
+module.exports = () => {
+  config()
+  const email = process.env.EMAIL
+  const pass = process.env.PASS
+  const url = process.env.NEXT_PUBLIC_POCKET_URL
+  console.log(email,pass,url);
+  const command = `npx pocketbase-typegen --url ${url} --email ${email} --password '${pass}' --out PocketBaseTypes/pocketbase-types.ts`;
+
+  execSync(command, {stdio: 'inherit'})
+}
+module.exports()
