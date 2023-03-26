fbc78533f8261fe1ae3f76d15e5f62983f7c8824
daniel-le97 <daniel97.le@gmail.com>
2023-03-26 11:47:48 -0600

    invalid auth

--- client/src/components/ServerOptions/LeaveServer.tsx
+++ client/src/components/ServerOptions/LeaveServer.tsx
@@ -1,21 +1,23 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { observer } from "mobx-react";
 import { useRouter } from "next/router";
-import { useUser } from "../../../utils/pocketBase";
+// import { useUser } from "../../../utils/pocketBase";
 import Pop from "../../../utils/Pop";
 import { membersService } from "../../services/MembersService";
 import type { Server } from "../../../PocketBaseTypes/utils";
 import React from "react";
 import { AppState } from "../../../AppState";
-const LeaveServer = ({ server }: { server: Server}) => {
+const LeaveServer = () => {
   const user = AppState.user
+  const server = AppState.activeServer
   const router = useRouter();
   const leaveServer = async () => {
     try {
       const data = {
-        server: server.id,
-        user: user!.id,
+        server: server?.id,
+        user: user?.id,
       };
 
       const yes = await Pop.confirm(`Leave ${server?.name}`,"Are You Sure?",'question','Confirm');
