bc8fff48754f3e83a48a68030038041486dc0bf1
Tung N Le <tung.le0319@gmail.com>
2023-04-13 22:00:09 -0600

    Finally beginning to tackle account Page

--- client/src/pages/AccountPage.tsx
+++ client/src/pages/AccountPage.tsx
@@ -5,7 +5,7 @@
 import { observer } from "mobx-react";
 import { NextPage } from "next";
 import type { Admin, Record } from "pocketbase";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,Fragment} from "react";
 import { useForm } from "react-hook-form";
 import { useUser } from "../../hooks/User";
 import type {
@@ -17,12 +17,35 @@ import { pb } from "../../utils/pocketBase";
 import Pop from "../../utils/Pop";
 import { accountsService } from "../services/AccountsService";
 import { membersService } from "../services/MembersService";
-import { Menu } from "@headlessui/react";
+import { Dialog, Menu,Transition } from "@headlessui/react";
 import { AppState } from "AppState";
 import router from "next/router";
+import UserAvatar from "@/components/GlobalComponents/UserAvatar";
+
 function AccountPage() {
   const user = AppState.user;
   const servers = AppState.userServers;
+
+
+const [hide, setHide] = useState(false);
+
+  const toggleHide = () => {
+    setHide(!hide);
+  };
+
+  const getEmailDisplay = () => {
+    if (hide) {
+      return user?.email;
+    } else {
+      const index = user?.email!.indexOf("@");
+      const domain = user?.email!.substring(index);
+      const username = user?.email!.substring(0, index);
+      const maskedUsername = "*".repeat(username.length);
+      return `${maskedUsername}${domain}`;
+    }
+  };
+
+
   useEffect(() => {
     if (!user) {
       router.push("/");
@@ -36,36 +59,42 @@ function AccountPage() {
     };
     fetchUserServers();
   }, []);
-  
 
   return (
-    <main className="  min-h-screen dark:bg-zinc-800  ">
-      <div className="account-page">
-        <div className="card  flex flex-col  justify-center   ">
-          <div className=" flex flex-col items-center justify-center p-5 text-center ">
-            <img
-              src={user?.avatarUrl}
-              alt="Profile Image"
-              className="h-32 w-32 rounded-full shadow-md shadow-zinc-900"
-            />
+    <main className="  min-h-screen w-full   dark:bg-zinc-800">
+      <div className="p-5 ">
+        <div className="text-xl font-bold text-gray-300">My Account</div>
+        <div className=" flex flex-col items-center justify-center p-5 text-center  text-gray-400 ">
+          <UserAvatar avatarUrl={user?.avatarUrl!} width="w-48" height="h-48" />
 
-            {user?.username}
-          </div>
-          <div className="card-body justify-center p-5 text-center">
-            {/* {JSON.stringify(user)} */}
-            <EditAccount />
-            
+          <div className=" mt-2 flex flex-col  gap-y-5 rounded bg-zinc-700 p-5 text-start text-xl font-bold">
+            <div className="flex flex-col">
+              <div className="text-sm">USERNAME</div>
+              <div className=""> {user?.username}</div>
+            </div>
+            <div className="flex flex-row items-center">
+              <div className="text-sm">EMAIL</div>
+              <div className="ml-2">{getEmailDisplay()}</div>
+              <button className="ml-2 text-sm text-indigo-500  " onClick={toggleHide}>
+                {hide ? "Show" : "Hide"}
+              </button>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-sm">MEMBER SINCE</div>
+              <div className=""> {user?.created}</div>
+            </div>
           </div>
         </div>
       </div>
+
+      <EditAccount />
     </main>
   );
 }
 
-
-
 const EditAccount = () => {
   const user = AppState.user;
+  const [isOpen, setIsOpen] = useState(false);
   const {
     register,
     handleSubmit,
@@ -92,43 +121,92 @@ const EditAccount = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex w-1/2 flex-col gap-y-3 text-white "
-    >
-      <div className="flex flex-col text-start">
-        <label>UserName:</label>
-        <input
-          type="text"
-          // onChange={handleChange}
-          {...register("username", {
-            minLength: 3,
-            maxLength: 30,
-            required: true,
-          })}
-          className="m-1 ml-3 rounded-sm bg-gray-300 p-1 text-black placeholder:text-gray-100 required:border-2 required:border-red-400"
-        />
-      </div>
-      <div className="flex flex-col text-start">
-        <label>AvatarUrl:</label>
-        <input
-          {...register("avatarUrl", {
-            required:true
-          })}
-          type="url"
-        
+    <div className=" flex w-full flex-col items-center justify-center">
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
 
-          className="m-1 ml-3 rounded-sm bg-gray-300 p-1 text-black placeholder:text-gray-100 required:border-2 required:border-red-400"
-        />
-      </div>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="dialog-modal">
+                  <Dialog.Title as="h3" className="dialog-title">
+                    Edit Account
+                  </Dialog.Title>
 
-      <button
-        type="submit"
-        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-      >
-        Submit
+                  <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="  flex flex-col gap-y-5 rounded p-5 text-gray-300"
+                  >
+                    <div className="flex flex-col text-start ">
+                      <label>UserName</label>
+                      <input
+                        type="text"
+                        {...register("username", {
+                          minLength: 3,
+                          maxLength: 30,
+                          required: true,
+                        })}
+                      />
+                    </div>
+                    <div className="flex flex-col text-start">
+                      <label>AvatarUrl</label>
+                      <input
+                        {...register("avatarUrl", {
+                          required: true,
+                        })}
+                        type="url"
+                      />
+                    </div>
+
+                    <div className="">
+                      <button type="submit" className="btn-primary">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="btn-secondary"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+      <button className="btn-primary" onClick={() => setIsOpen(true)}>
+        {" "}
+        Edit
       </button>
-    </form>
+    </div>
   );
 };
 export default observer(AccountPage);
